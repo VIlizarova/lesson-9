87981884d0ece5a4b5b98d511614f18787a49b69
Valeria_Ilizarova <v.ilizarova@gmail.com>
2022-09-22 15:19:51 +0300

    tasks 64 66

---
+++ C#/lesson 9 homework/task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine ("Введите значение числа m");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите значение числа n");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = m;
+while (n !=m)
+{
+    sum = sum + (m+1);
+    m++; 
+}
+
+Console.WriteLine(sum);
